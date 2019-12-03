68ecf38c2a875ececd7cca481dfded6119d8dd50
Axel Fahy <axel.fahy@heig-vd.ch>
2019-12-03 11:55:03 +0100

    feat(screenlayout): change primary monitor

--- screenlayout-narwhal/.screenlayout/normal.sh
+++ screenlayout-narwhal/.screenlayout/normal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-1 --off --output HDMI-0 --off --output DP-3 --off --output DP-2 --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-0 --mode 1920x1200 --pos 0x192 --rotate normal
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-3 --off
