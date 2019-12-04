6d16c6fcaee865e4cc6cef5b66bfe9708e667f89
Axel Fahy <axel.fahy@heig-vd.ch>
2019-12-04 09:42:14 +0100

    feat(polybar): launch system tray on primary monitor

--- polybar-common/.config/polybar/launch.sh
+++ polybar-common/.config/polybar/launch.sh
@@ -6,6 +6,7 @@
 #         USAGE: ./launch.sh
 #
 #   DESCRIPTION: Script to launch polybar on all monitors.
+#                The system tray is only run on the primary monitor.
 #
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
@@ -17,19 +18,27 @@
 #      REVISION:  ---
 #===============================================================================
 
-set -o nounset                              # Treat unset variables as an error
+# Treat unset variables as an error.
+set -o nounset
 
-# Terminate already running bar instances
+# Terminate already running bar instances.
 killall -q polybar
 
-# Wait until the processes have been shut down
+# Wait until the processes have been shut down.
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch all bars
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     echo "Lauching bar on $m"
+    # Check if this is the primary monitor.
+    if [[ $(polybar --list-monitors | grep $m | cut -d "(" -f2 | cut -d ")" -f1) == "primary" ]]; then
+        echo "$m is the primary monitor"
+        tray=right
+    else
+        tray=none
+    fi
     # Change `/dev/null` to `/tmp/polybar_$m.log` to have the log.
-    MONITOR=$m polybar --reload -c $HOME/.config/polybar/config bottom 2>/dev/null &
+    MONITOR=$m TRAY_POSITION=$tray polybar --reload -c $HOME/.config/polybar/config bottom 2>/dev/null &
 done
 
 echo "Bars launched..."
