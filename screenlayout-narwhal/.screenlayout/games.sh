257bee509235043266d561a867053d38d06f405d
Axel Fahy <axel@fahy.net>
2022-08-28 11:55:33 +0200

    fix(screenlayout): fix screenlayouts for narwhal

--- screenlayout-narwhal/.screenlayout/games.sh
+++ screenlayout-narwhal/.screenlayout/games.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-1 --off --output HDMI-0 --off --output DP-3 --off --output DP-2 --mode 3440x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-0 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
--- screenlayout-narwhal/.screenlayout/normal.sh
+++ screenlayout-narwhal/.screenlayout/normal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-3 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1200 --pos 3440x240 --rotate normal --output DP-3 --off
